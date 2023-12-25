f6d913736ab3d1a3c9ac2ad8617d553bdda734d2
roomb502 <154802160+roomb502@users.noreply.github.com>
2023-12-25 16:25:30 +0530

    added

---
+++ script_nev.js
@@ -0,0 +1,9 @@
+function home(){
+    window.location.href = 'welcome.html';
+}
+function logout(){
+    window.location.href = 'index.html';
+}
+function news(){
+    window.location.href = 'news.html';
+}
